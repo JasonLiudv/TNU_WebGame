cc5d3e830cf5cc4ab60a073dfa2307acf8bf9b15
JasonLiudv <79687903+JasonLiudv@users.noreply.github.com>
2022-03-22 16:05:02 +0800

    0322

--- TNU_WebGame/Assets/Scripts/TopDownController.cs
+++ TNU_WebGame/Assets/Scripts/TopDownController.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 namespace Jason
 {
+    /// <summary>
+    /// 角色控制器：Top Down 類型
+    /// </summary>
     public class TopDownController : MonoBehaviour
     {
         #region Data
+        [SerializeField, Header("移動速度"), Range(0, 100)]
         private float speed = 10.5f;
         private string parameterRun = "開關跑步";
         private string parameterDead = "開關死亡";
         private Animator ani;
         private Rigidbody2D rig;
+        private float h;
+        private float v;
         #endregion
 
         #region Event
@@ -22,11 +28,29 @@ namespace Jason
 
         private void Update()
         {
-            
+            GetInput();
+            Move();
         }
         #endregion
 
         #region Method
+        /// <summary>
+        /// 取得玩家輸入資料
+        /// 上下左右
+        /// </summary>
+        private void GetInput()
+        {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+        }
+
+        private void Move()
+        {
+            rig.velocity = new Vector2(h, v) * speed;
+            ani.SetBool(parameterRun, h != 0 || v != 0);
+
+            transform.eulerAngles = new Vector3(0, h >= 0 ? 0 : 180, 0);
+        }
         #endregion
     }
 }
